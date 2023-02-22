2c622aca303f8cdf65de6e01d1b1baa6a2ad2532
Xi-Yuer <2214380963@qq.com>
2023-02-22 20:08:23 +0800

    状态管理

---
+++ lib/store/view_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CounterViewModel extends ChangeNotifier {
+  int _counter = 100;
+
+  int get counter => _counter;
+
+  set counter(int value) {
+    _counter = value;
+    /// 通知所有的依赖组件进行视图更新
+    notifyListeners();
+  }
+}
